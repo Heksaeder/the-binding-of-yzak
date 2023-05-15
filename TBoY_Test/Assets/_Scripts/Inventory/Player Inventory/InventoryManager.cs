cff161b9cee1e72e4c77ffbca6553aabfe263339
Maximilian <61150071+MaxPW777@users.noreply.github.com>
2023-05-16 00:14:35 +0200

    fix

--- TBoY_Test/Assets/_Scripts/Inventory/Player Inventory/InventoryManager.cs
+++ TBoY_Test/Assets/_Scripts/Inventory/Player Inventory/InventoryManager.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
+
 public class InventoryManager : MonoBehaviour
 {
     public static InventoryManager Instance;
+
     public List<Item> Items = new List<Item>();
-public GameObject Inventory;
+
+    private GameObject Inventory;
+
     public Transform ItemContent;
+
     public GameObject InventoryItem;
+
     private void Awake()
     {
         Inventory = GameObject.Find("Inventory");
@@ -19,18 +25,20 @@ public GameObject Inventory;
     // Update is called once per frame
     public void TransferItem(Item item)
     {
-        Items.Add(item);
-        ChestManager.Instance.Remove(item);
+        Items.Add (item);
+        ChestManager.Instance.Remove (item);
     }
 
     public void AddItem(Item item)
     {
-        Items.Add(item);
+        Items.Add (item);
     }
+
     public void RemoveItem(Item item)
     {
-        Items.Remove(item);
+        Items.Remove (item);
     }
+
     public void ListItems()
     {
         ClearItems();
@@ -38,17 +46,20 @@ public GameObject Inventory;
         {
             GameObject obj = Instantiate(InventoryItem, ItemContent);
             var itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
-            var itemName = obj.transform.Find("ItemName").GetComponent<TextMeshProUGUI>();
+            var itemName =
+                obj.transform.Find("ItemName").GetComponent<TextMeshProUGUI>();
 
             itemName.text = item.itemName;
             itemIcon.sprite = item.itemImage;
         }
     }
+
     public void OpenInventory()
-    {  
+    {
         ListItems();
         Inventory.SetActive(true);
     }
+
     public void ClearItems()
     {
         foreach (Transform child in ItemContent)
